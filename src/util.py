370bfcf7060d789010d563e3f0c80d73614a25ca
masato <spmt3710@gmail.com>
2023-08-01 21:33:23 +0900

    add : read_parquet_from_csv

--- src/util.py
+++ src/util.py
@@ -2,7 +2,12 @@ import json
 import shutil
 from pathlib import Path
 
+import pandas as pd
 import yaml
+
+from src.logger import Logger
+
+logger = Logger()
 
 
 def load_yaml(filepath: Path) -> dict:
@@ -34,3 +39,16 @@ def load_json(filepath: Path) -> dict:
     with open(filepath) as f:
         dic = json.load(f)
     return dic
+
+
+def read_parquet_from_csv(filepath: Path, dirpath: Path) -> pd.DataFrame:
+    name = filepath.name.split(".")[0]
+    parquet_filepath = dirpath / f"{name}.parquet"
+    if parquet_filepath.is_file():
+        logger.info("load parquet file")
+        return pd.read_parquet(parquet_filepath)
+
+    logger.info("load csv & convert to parquet")
+    df = pd.read_csv(filepath)
+    df.to_parquet(parquet_filepath)
+    return df
