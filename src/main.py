b6451b2cbd85331af23b3c8d5a45a589c5bf1953
osushinekotan <spmt3710@gmail.com>
2023-09-04 16:06:48 +0900

    add : click

--- src/main.py
+++ src/main.py
@@ -1,11 +1,24 @@
-def v01():
+import click
+
+
+@click.command()
+@click.option("--fe", is_flag=True, help="Run fe.run() if set.")
+@click.option("--train", is_flag=True, help="Run train.run() if set.")
+@click.option("--inference", is_flag=True, help="Run inference.run() if set.")
+@click.option("--make_submission", is_flag=True, help="Run make_submission.run() if set.")
+def run_v01(**kwargs):
+    """Run selected functions based on the provided flags."""
     from custom.runner.v01 import fe, inference, make_submission, train
 
-    fe.run()
-    train.run()
-    inference.run()
-    make_submission.run()
+    if kwargs.get("fe"):
+        fe.run()
+    if kwargs.get("train"):
+        train.run()
+    if kwargs.get("inference"):
+        inference.run()
+    if kwargs.get("make_submission"):
+        make_submission.run()
 
 
 if __name__ == "__main__":
-    v01()
+    run_v01()
