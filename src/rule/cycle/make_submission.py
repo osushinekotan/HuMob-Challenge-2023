0f889aab30ac723dc6cb9bc7ea221464ce32e713
osushinekotan <spmt3710@gmail.com>
2023-09-09 14:45:16 +0900

    add : make submission (rule base)

--- src/rule/cycle/make_submission.py
+++ src/rule/cycle/make_submission.py
@@ -1,18 +1,20 @@
+from pathlib import Path
+
 import pandas as pd
 from pytorch_pfn_extras.config import Config
 from rule.cycle.evaluate import make_preds_df
 from rule.cycle.imputer import CycleImputer
-from util import load_yaml
 
 
-def run():
-    config = load_yaml("/workspace/src/conf/rule.yaml")
-    config = Config(config=config)
+def run(pre_eval_config):
+    config = Config(config=pre_eval_config)
     task_dataset = config["cycle/task_dataset"]
 
-    filepath = f"/workspace/resources/input/{task_dataset}_raw_train.parquet"
+    filepath = f"/workspace/resources/input/{task_dataset}_raw_test.parquet"
 
     df = pd.read_parquet(filepath)
+    print(df)
+
     imputer = CycleImputer(group_keys=config["cycle/group_keys"], agg_method=config["cycle/agg_method"])
     preds_df = make_preds_df(
         df,
@@ -21,8 +23,13 @@ def run():
         cycle_groups=config["cycle/cycle_groups"],
         T=config["cycle/T"],
     )
-    print(preds_df)
+    generated = preds_df[["uid", "d", "t"] + imputer.agg_cols]
+    generated.columns = ["uid", "d", "t", "x", "y"]
 
+    print(generated)
 
-if __name__ == "__main__":
-    run()
+    out_dir = Path(f"/workspace/resources/output/rule/{config['cycle/name']}")
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    submission_filepath = out_dir / f"osushineko_{task_dataset[:5]}_humob.csv.gz"
+    generated.to_csv(submission_filepath, index=False)
